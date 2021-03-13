4e615c92830abd9919dade780f5f410bdf14aa5c
MacBook Pro <macbookpro@192.168.1.100>
2021-03-13 09:35:59 -0500

    advance detail product

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home, ProductSearch, DetailProduct } from 'pages';
+import { Home, ProductSearch, DetailProduct } from 'views';
 
 const Routes = () => (
   <Router>
