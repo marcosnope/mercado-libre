06a90a6a70ae87c1ff8fb6fe4e91a3cebc490866
MacBook Pro <macbookpro@192.168.1.100>
2021-03-13 11:12:38 -0500

    detail

--- src/views/detailProduct/DetailProduct.js
+++ src/views/detailProduct/DetailProduct.js
@@ -1,10 +1,71 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { Header, BreadCrumb } from 'components';
+import { Container, Row, Col } from 'react-bootstrap';
+import classNames from 'classnames/bind';
+import styles from './DetailProduct.module.scss';
+
+const cx = classNames.bind(styles);
+
+const migaDePan = [
+  { title: 'Electronica, Audio y Video' },
+  { title: 'iPod' },
+  { title: 'Reproductores' },
+  { title: 'iPod touch' },
+  { title: '32 GB' },
+];
+
+const product = {
+  image: 'https://http2.mlstatic.com/D_677004-MLA31971773611_082019-O.jpg',
+  caracteristic: '',
+  name: '',
+  price: '',
+  description: '',
+};
 
 function DetailProduct() {
-  const { id } = useParams();
+  // const { id } = useParams();
 
-  return <div>ID: {id}</div>;
+  return (
+    <section>
+      <Header />
+      <BreadCrumb items={migaDePan} />
+      <Container>
+        <Container fluid className={cx('content')}>
+          <Row>
+            <Col xs={8} sm={8} md={8} className="p-0">
+              <div className={cx('box-image')}>
+                <img src={product.image} alt="product" />
+              </div>
+            </Col>
+            <Col xs={4} sm={4} md={4} className="p-0">
+              <div className={cx('box-price')}>
+                <p>Nuevo 234 vendidos</p>
+                <h4>Deco Reverse Sobrero Oxford</h4>
+                <h1>$ 1.980</h1>
+                <button>Comprar</button>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={12} md={8} className="p-0">
+              <div className={cx('box-description')}>
+                <h3>Descripción del producto</h3>
+                <p>
+                  Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus
+                  in massa egestas mollis varius; dignissim elementum. Mollis
+                  tincidunt mattis hendrerit dolor eros enim, nisi ligula
+                  ornare. Hendrerit parturient habitant pharetra rutrum gravida
+                  porttitor eros feugiat. Mollis elit sodales taciti duis
+                  praesent id. Consequat urna vitae morbi nunc congue.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
 }
 
 export default DetailProduct;
