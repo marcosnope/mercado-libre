eef4fed4546365313709949fa4232c0ba345ff0f
marcosnope <38289995+marcosnope@users.noreply.github.com>
2021-03-12 19:20:48 -0500

    structure dashboard (#1)
    Co-authored-by: MacBook Pro <macbookpro@192.168.1.100>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,28 +2,27 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FormControl } from 'components';
 import { logo } from 'assets/images';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Header = (_) => {
   return (
-    <Container>
-      <Row>
-        <Col xs={2} sm={2} md={1}>
-          <div
-            style={{
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <img src={logo} alt="logotipo" width="60" />
-          </div>
-        </Col>
-        <Col xs={10} sm={10} md={11}>
-          <FormControl />
-        </Col>
-      </Row>
-    </Container>
+    <header>
+      <Container>
+        <Row>
+          <Col xs={2} sm={2} md={1}>
+            <div className={cx('box-logo')}>
+              <img src={logo} alt="logotipo" width="60" />
+            </div>
+          </Col>
+          <Col xs={10} sm={10} md={11}>
+            <FormControl />
+          </Col>
+        </Row>
+      </Container>
+    </header>
   );
 };
 
